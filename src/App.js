7d885eded7d5a80277088cc9412b518ddcfd2db8
doosemavis <moosedavis2011@gmail.com>
2021-01-12 12:33:45 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -15,15 +15,6 @@ class App extends Component {
           <div id="seperator"></div>
           <div className="wrapper">
             <ColorBox opacity={1} />
-            <ColorBox opacity={2} />
-            <ColorBox opacity={3} />
-            <ColorBox opacity={4} />
-            <ColorBox opacity={5} />
-            <ColorBox opacity={6} />
-            <ColorBox opacity={7} />
-            <ColorBox opacity={8} />
-            <ColorBox opacity={9} />
-            <ColorBox opacity={10} />
           </div>
       </div>
     )
